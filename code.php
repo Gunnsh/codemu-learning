1e944434990c5a15e0aa54f980c735e4c33731c8
Gunnsh <radio_art@mail.ru>
2022-06-08 17:51:01 +0700

    end of basic, if..else

--- code.php
+++ code.php
@@ -1,5 +1,20 @@
 <?php
-$tf = 68;
-$tc = (5/9) * ($tf - 32);
-echo $tc;
-?>
+	$a = 222222;
+
+	$b = str_split("$a");
+	$sum = 0;
+	$tum = 0;
+	foreach ($b as $key => $values) {
+		if ($key <=2) {
+			$sum += $values;
+		}
+		if ($key >2) {
+			$tum += $values;
+		}
+	}
+	
+	if ($sum == $tum) {
+		echo "Задача решена правильно!";
+	} else {
+		echo "Что то идет не так :(";
+	}
