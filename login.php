a0e0051224fc63533d50eb62e67d7fb22f1aa79e
Gunnsh <radio_art@mail.ru>
2022-06-30 12:06:17 +0700

    done hash passwords, changed to normal html sign in and sign up

--- login.php
+++ login.php
@@ -1,39 +1,96 @@
-<?php 
-session_start();
+<?php session_start(); ?>
 
-if (empty($_POST)) { ?>
-<h2>Вы не авторизованы!</h2>
-<form action="" method="POST">
-	<p>Введите логин</p>
-	<input name="name">
-	<p>Введите пароль</p>
-	<input name="password" type="password">
-	<input type="submit">
-</form>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>Авторизация</title>
+		<style>
+			.error {color: #FF0000;}
+		</style>
+	</head>
+<body>
+
 <?php
-}
-if (!empty($_POST['name']) and !empty($_POST['password'])) {
-	$login = $_POST['name'];
-	$password = $_POST['password'];
+$login = $pass = "";
+$loginErr = $passErr = $autoErr = "";
 
-	$host = 'localhost';
-	$user = 'root';
-	$pass = 'root';
-	$name = 'mydb';
-
-	$link = mysqli_connect($host, $user, $pass, $name); // присваиваем переменной обьект соединения с базой данных
-	mysqli_query($link, "set names 'utf8'");
-	$query = "SELECT * FROM users WHERE name='$login' AND password='$password'";
-	$result = mysqli_query($link, $query);
-	$row = mysqli_fetch_assoc($result);
-
-	if (!empty($row)) {
-		$_SESSION['auth'] = true;
-		$_SESSION['login'] = $login;
-		header("Location: 1.php");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if (empty($_POST["login"])) {
+		$loginErr = "Введите логин";
 	} else {
-		echo "Неверно введен логин или пароль";
-	};
-};
+		$login = test_input($_POST["login"]);
+	}
+
+	if (empty($_POST["pass"])) {
+		$passErr = "Введите пароль";
+	} else {
+		$pass = $_POST["pass"];
+	}
+}
+
+if (!empty($_SESSION["flash"])) {
+	$autoErr = $_SESSION["flash"];
+	unset($_SESSION["flash"]);
+}
+
+function test_input($date) {
+	$date = trim($date);
+	$date = stripslashes($date);
+	$date = htmlspecialchars($date);
+	return $date;
+}
 ?>
-Хотите <a href="register.php">зарегистрироваться</a>?
+
+<form action="<?= $_SERVER["PHP_SELF"]?>" method="POST">
+	<table align="center">
+		<tr>
+			<td></td>
+			<td><span class="error">* обязательные поля</span></td>
+		</tr>
+		<tr>
+			<td align="right">Логин: </td>
+			<td><input name="login" value="<?php if (isset($_POST["login"])) echo $login; ?>">
+			<span class="error">* <?= !empty($autoErr) ? $autoErr : $loginErr ?></span></td>
+		</tr>
+		<tr>
+			<td align="right">Пароль: </td>
+			<td><input type="password" name="pass" value="<?php if (isset($_POST["pass"])) echo $pass; ?>">
+			<span class="error">* <?= $passErr ?></span></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td><input type="submit" value="Отправить"> <a href="register.php">Зарегистрироваться</a></td>
+		</tr>
+	</table>
+</form>
+</body>
+
+<?php
+if (($loginErr && $passErr && $autoErr) == "") {
+	$host = "localhost";
+	$user = "root";
+	$tablepass = "root";
+	$db = "mydb";
+
+	$link = mysqli_connect($host, $user, $tablepass, $db);
+
+	if (($login && $pass) != "") {
+//		$pass = md5($pass);
+		$query = "SELECT * FROM users WHERE name='$login'";
+		$resquery = mysqli_fetch_assoc(mysqli_query($link, $query));
+
+		if ($resquery !== null) {
+			$hash = $resquery["password"];
+
+			if (password_verify($_POST["pass"], $hash)) {
+				echo "Авторизация прошла успешно!";
+			} else {
+				$_SESSION["flash"] = "Введены неправильные данные";
+			}
+		} else {
+			$_SESSION["flash"] = "Введены неправильные данные";
+		}
+	}
+}
+?>
